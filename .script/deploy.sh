8ee5fafcf5e986c2c8a7d37ae194840978942e5a
vakratundsolutions2 <156416578+vakratundsolutions2@users.noreply.github.com>
2024-01-17 11:32:31 +0530

    Update deploy.sh

--- .script/deploy.sh
+++ .script/deploy.sh
@@ -10,4 +10,8 @@ echo "New changes copied to server !"
 echo "Installing Dependencies..."
 npm install --yes
 
+npm run build
+
+pm2 start npm --name \"test_server\" --start
+
 echo "Deployment Finished!"
